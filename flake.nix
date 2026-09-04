c4316af57e717f285c9cb4c364f4becad8288f72
philomagi <philomagi-contact@philomagi.dev>
2026-09-05 02:45:32 +0900

    setup herdr

--- flake.nix
+++ flake.nix
@@ -8,9 +8,17 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    iwaya = {
+      url = "github:tooppoo/iwaya/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    herdr = {
+      url = "github:herdrdev/herdr/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, iwaya, herdr, ... }:
     {
       nixosConfigurations.philomagi-homelab = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -22,6 +30,12 @@
             home-manager.useGlobalPkgs = true;
             home-manager.users.philomagi = import ./home.nix;
           }
+          ({ pkgs, ... }: {
+            environment.systemPackages = [
+              iwaya.packages.${pkgs.system}.default
+              herdr.packages.${pkgs.system}.default
+            ];
+          })
         ];
       };
     };
